4973eb4f01e05b0ae5d111c95537d8523b19c22e
AnnDest <anna_dest@mail.ru>
2013-07-07 16:59:29 +0200

    project
    =)

---
+++ FirstProject/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MovingObject : MonoBehaviour 
+{
+	public List<GameObject> waypoints;
+	
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+	
+	
+}
---
+++ FirstProject/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class WayPoint : MonoBehaviour {
+	
+	public int number = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
