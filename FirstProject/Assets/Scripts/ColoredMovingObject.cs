58cd40ee26631ba1979e92e57f9776766b0c2e82
AnnDest <anna_dest@mail.ru>
2013-07-08 18:17:36 +0200

    nothing new

--- FirstProject/Assets/Scripts/ColoredMovingObject.cs
+++ FirstProject/Assets/Scripts/ColoredMovingObject.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class ColoredMovingObject : MovingObject 
 {
-	void Update()
+	private float colorChangeSpeed = 0.2f;
+
+	float T(float time)
+	{
+		float t = Mathf.Abs(Mathf.Sin(time * colorChangeSpeed * Mathf.PI));	
+		return t;
+	}
+	
+	new void Update()
 	{
 		base.Update();
-		MeshRenderer gameObjectRenderer = gameObject.GetComponent<MeshRenderer>();
-		gameObjectRenderer.material.color = Color.Lerp(Color.red, Color.blue, Time.time);
+		MeshRenderer gameObjectRenderer = gameObject.GetComponent<MeshRenderer>();		
+		gameObjectRenderer.material.color = Color.Lerp(Color.red, Color.blue, T(Time.time));
+		gameObject.GetComponentsInChildren<MeshRenderer>()[1].material.color = gameObjectRenderer.material.color;
 	}
 }
